695ee9a6776dd03402ac61fef2babb6d0911105d
ferdistro <ferdinand.strobel@infront.co>
2025-07-17 17:57:15 +0200

    add HotReloada

--- src/main/java/com/engine/HotReload.java
+++ src/main/java/com/engine/HotReload.java
@@ -1,5 +1,11 @@
 package com.engine;
 
+import com.engine.Utils.Coords2D;
+import com.engine.Utils.Coords3D;
+import com.engine.Utils.ScreenObject;
+import com.engine.Utils.Triangle;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import org.joml.Math;
 import org.joml.Matrix4f;
 import org.lwjgl.BufferUtils;
@@ -9,12 +15,10 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.lang.reflect.Type;
 import java.nio.FloatBuffer;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
 
 import static org.lwjgl.glfw.GLFW.*;
 import static org.lwjgl.opengl.GL11.GL_TRUE;
@@ -29,11 +33,12 @@ public class HotReload {
     private static final String vertexShaderURL = "/home/ferdistro/Projects/Private/OpenGL-Engine/src/main/java/com/engine/shaders/test_vertex.vert";
     private static final String fragmentShaderURL = "/home/ferdistro/Projects/Private/OpenGL-Engine/src/main/java/com/engine/shaders/test_fragment.frag";
     private static final String verticaslURL = "/home/ferdistro/Projects/Private/OpenGL-Engine/src/main/java/com/engine/shaders/3DVerticals";
-    private float angle = 0f;
+    private static final String jsonCoordinateList = "src/main/java/com/engine/shaders/Cords.json";
+    private final Gson gson = new Gson();
+    private final float angle = 0f;
+    float moveX = 0;
     private long window;
     private int shaderProgram, vao;
-
-
     private HotReloadUpdate hotReloadInterface;
 
     public static void main(String[] args) {
@@ -61,36 +66,50 @@ public class HotReload {
     }
 
     private void loop() {
-        while (!glfwWindowShouldClose(window))
-        {
+        while (!glfwWindowShouldClose(window)) {
+
+            glClearColor(0f, 0f, 0f, 1f);
+            glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+            glUseProgram(shaderProgram);
+
+            glDisable(GL_CULL_FACE);
+            glEnable(GL_DEPTH_TEST);
+
 
             //not effective but works
             hotReloadInterface.reload();
 
+            //Move Object
+            moveX = moveX + 0.1f;
+            if(moveX > 5){
+                moveX = -5.0f;
+            }
 
-            glClearColor(0f, 0f, 0f, 1f);
-            glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-            glEnable(GL_DEPTH_TEST);
-
-            glUseProgram(shaderProgram);
 
             // Update Transformation Matrices
 //            angle += 0.01f;
-            Matrix4f model = new Matrix4f().rotateY(angle).rotateX(angle * 0.5f);
-            Matrix4f view = new Matrix4f().translate(0, 0, -3);
+
+            //Camera distance
+            Matrix4f view = new Matrix4f().translate(0, 0, -10);
             Matrix4f projection = new Matrix4f().perspective(Math.toRadians(45.0f), 800f / 600f, 0.1f, 100f);
+            Matrix4f model = new Matrix4f().rotateY(angle).rotateX(angle);
 
             int modelLoc = glGetUniformLocation(shaderProgram, "model");
             int viewLoc = glGetUniformLocation(shaderProgram, "view");
             int projLoc = glGetUniformLocation(shaderProgram, "projection");
 
-            FloatBuffer fb = BufferUtils.createFloatBuffer(16);
-            glUniformMatrix4fv(modelLoc, false, model.get(fb));
-            glUniformMatrix4fv(viewLoc, false, view.get(fb));
-            glUniformMatrix4fv(projLoc, false, projection.get(fb));
+            FloatBuffer modelFb = BufferUtils.createFloatBuffer(16);
+            FloatBuffer viewFb = BufferUtils.createFloatBuffer(16);
+            FloatBuffer projFb = BufferUtils.createFloatBuffer(16);
+
+
+            glUniformMatrix4fv(modelLoc, false, model.get(modelFb));
+            glUniformMatrix4fv(viewLoc, false, view.get(viewFb));
+            glUniformMatrix4fv(projLoc, false, projection.get(projFb));
 
             glBindVertexArray(vao);
-            glDrawArrays(GL_TRIANGLES, 0, 36);
+            glDrawArrays(GL_TRIANGLES, 0, 9);
 
             glfwSwapBuffers(window);
             glfwPollEvents();
@@ -103,6 +122,8 @@ public class HotReload {
         glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
         glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
         glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+
         window = glfwCreateWindow(800, 600, "Rotating Cube", NULL, NULL);
 
 
@@ -115,17 +136,14 @@ public class HotReload {
 
         try {
 
-            this.hotReloadInterface = new HotReloadUpdate() {
-                @Override
-                public void reload() {
-                    try {
-                        loadShaderProgram();
-                        loadVerticalsObject();
-                    } catch (Exception e) {
-                        //ignore
-                    }
-
+            this.hotReloadInterface = () -> {
+                try {
+                    loadShaderProgram();
+                    loadVerticalsObject();
+                } catch (Exception e) {
+                    //ignore
                 }
+
             };
 
 
@@ -142,20 +160,31 @@ public class HotReload {
 
     }
 
-
     private void loadVerticalsObject() throws IOException {
-        String s = loadFile(verticaslURL);
+        String json = loadFile(jsonCoordinateList);
+        Type listType = new TypeToken<ArrayList<Coords2D>>() {
+        }.getType();
+        ArrayList<Coords2D> coordList = new Gson().fromJson(json, listType);
 
-        List<Float> vertices = new ArrayList<>();
 
-        Arrays.stream(s.split(",")).forEach(x -> vertices.add(Float.parseFloat(x)));
-
-        Float[] verticesFloat = vertices.toArray(Float[]::new);
-
-        float[] primitiveArray = new float[verticesFloat.length];
-        for (int i = 0; i < verticesFloat.length; i++) {
-            primitiveArray[i] = verticesFloat[i];
+        List<Triangle> triangles = new ArrayList<>();
+        Coords3D[] coords3DS = new Coords3D[3];
+        for (int i = 0; i < coordList.size(); i++) {
+            Coords2D coords2D = coordList.get(i);
+            Coords3D coords3D = new Coords3D(coords2D.getSign(), coords2D.getTriangle(), coords2D.getX(), coords2D.getY(), coords2D.getZ());
+            coords3DS[i % 3] = coords3D;
+            if ((i + 1) % 3 == 0) {
+                Triangle triangle = new Triangle(coords3D.getTriangle());
+                triangle.setPoints(coords3DS);
+                triangles.add(triangle);
+            }
         }
+
+        ScreenObject screenObject = new ScreenObject(triangles);
+
+        screenObject.moveX(moveX);
+
+        float[] primitiveArray = screenObject.toPrimitiveArray();
 
 
         vao = glGenVertexArrays();
@@ -164,6 +193,7 @@ public class HotReload {
         glBindVertexArray(vao);
         glBindBuffer(GL_ARRAY_BUFFER, vbo);
         glBufferData(GL_ARRAY_BUFFER, primitiveArray, GL_STATIC_DRAW);
+
         glVertexAttribPointer(0, 3, GL_FLOAT, false, 3 * Float.BYTES, 0);
         glEnableVertexAttribArray(0);
 
